cc8f116edb430dc816fbe1c3d4f8da2371d94f6a
Derek Ren <guanguangua@gmail.com>
2020-04-15 12:44:33 -0400

    resolve merge conflict

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
-async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
-}
 /**
  * The html rewritter
  * @type {HTMLRewriter}
